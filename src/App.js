e0399694879fcfb476ac9adbfa21b5ca0a38ebc1
Greg Silvia <144576416+GregSilvia@users.noreply.github.com>
2023-11-05 21:30:39 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import BoxContainer from './BoxContainer.jsx';
+function App() {
+  return (
+    <div className="App">
+      <h1 className='title'>Fortune Teller</h1>
+      <BoxContainer />
+    </div>
+  );
+}
+
+export default App;
